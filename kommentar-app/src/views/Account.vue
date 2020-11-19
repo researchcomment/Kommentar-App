7eba8f92a43ab0d0a14da31ac5dbdd27b6aed50d
1itttlesheep <shizuojun99@gmail.com>
2020-11-19 17:17:01 +0100

    add style

--- kommentar-app/src/views/Account.vue
+++ kommentar-app/src/views/Account.vue
@@ -1,24 +1,50 @@
 <template>
-    <div>
-      <accounticon></accounticon>
-      or
-      <login></login>
-      
+  <div class="bar">
+    <div class="account">
+      <accounticon class="icon"></accounticon>
+    <login class="login"></login>
     </div>
-
+    
+  </div>
 </template>
 
 <script>
-import accounticon from '@/components/account/accounticon'
-import login from '@/components/login/login'
+import accounticon from "@/components/account/accounticon";
+import login from "@/components/login/login";
 export default {
-    components:{
-      accounticon,
-      login,
-    },
-}
+  components: {
+    accounticon,
+    login,
+  },
+};
 </script>
 
 <style>
+.bar {
+  width: 100%;
+  height: 40px;
+  border: 1px solid blue;
+}
+.account{
+  float: right;
+  margin-right: 20px;
+}
 
+
+.icon {
+  position:relative;
+  
+  border: 1px solid pink;
+  width: 100px;
+  height: 40px;
+}
+
+.login {
+  position: relative;
+  top: -40px;
+  border: 1px solid yellow;
+  width: 100px;
+  height: 40px;
+  display: none;
+}
 </style>
--- kommentar-app/src/views/Home.vue
+++ kommentar-app/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-       Home
+    <div class="search">
+       <div class="logo">Logo</div>
        <searchBar></searchBar>
 
     </div>
@@ -23,5 +23,21 @@
 </script>
 
 <style>
+    .search{
+        width: 800px;
+        height: 100px;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        margin-top: -50px;
+        margin-left: -400px;
+        border: 1px solid blue;
+        
+    }
+    .logo{
+        margin: 0 auto;
+        text-align: center;
+        font-size: 50px;
+    }
 
 </style>
