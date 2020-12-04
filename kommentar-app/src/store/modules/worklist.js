a33e10748cf0dc8916f10eafedeb745b7461ed9d
ahfie <49044940+ahfie@users.noreply.github.com>
2020-12-04 16:58:11 +0100

    result cached

--- kommentar-app/src/store/modules/worklist.js
+++ kommentar-app/src/store/modules/worklist.js
@@ -7,8 +7,9 @@ const state = () => ({
 const url = "http://api.crossref.org/works?query=";
 const getters = {}
 
-var last_keyword = "";
 var items = [];
+var cache = 100;
+var result_length = 0;
 
 function isNull(obj) {
     if (obj) {
@@ -30,50 +31,85 @@ function construct_author(author_ref) {
     author = author.join(" ; ");
     return author;
 }
-//async function return promise
-async function get_worklist(keyword, rows, offset, date) {
-    //convert keyword in this format list : keyword1+keyword2+...
-    keyword = keyword.split(" ");
-    keyword = keyword.join("+");
-    let datefrom = date.from.getFullYear() + "-" +
-        ((date.from.getMonth() + 1) < 10 ? "0" : "") + (date.from.getMonth() + 1) +
-        "-" + (date.from.getDate() < 10 ? "0" : "") + date.from.getDate();
-    let dateto = date.to.getFullYear() + "-" +
-        ((date.to.getMonth() + 1) < 10 ? "0" : "") + (date.to.getMonth() + 1) + "-" +
-        (date.to.getDate() < 10 ? "0" : "") + date.to.getDate();
-    var search_url = url + keyword + "&filter=from-update-date:" + datefrom +
-        ",until-update-date:" + dateto +
-        "&rows=" + 100 + "&offset=" + offset;
-    console.log(search_url);
-    let returnValue = {
-        list: [],
-        length: 0
-    }
-    return axios.get(search_url).then(res => {
-        //get reference : res -> data -> message
-        console.log(res);
-        let ref = res.data.message;
-        if (!isNull(ref)) {
-            for (var i = 0; i < ref.items.length; i++) {
-                //actural reference of result list from crossref
-                var item_ref = ref.items[i];
-                //construct info which needed to be return
-                var _title = !isNull(item_ref.title) ? item_ref.title[0] : "unkown";
-                var _author = !isNull(item_ref.author) ? construct_author(item_ref.author) : "unkown";
-                var _doi = !isNull(item_ref.DOI) ? item_ref.DOI : "unkown";
-                returnValue.list.push({
-                    title: _title,
-                    author: _author,
-                    doi: _doi
-                });
-            }
-            returnValue.length = ref["total-results"];
 
+function cons_returnValue(returnValue, from, to) {
+    if (items[to]) {
+        for (var i = from; i < to; i++) {
+            //actural reference of result list from crossref
+            var item_ref = items[i];
+            //construct info which needed to be return
+            var _title = !isNull(item_ref.title) ? item_ref.title[0] : "unkown";
+            var _author = !isNull(item_ref.author) ? construct_author(item_ref.author) : "unkown";
+            var _doi = !isNull(item_ref.DOI) ? item_ref.DOI : "unkown";
+            returnValue.list.push({
+                title: _title,
+                author: _author,
+                doi: _doi
+            });
         }
         return returnValue;
-    }).catch(err => {
-        console.log(err);
-    });
+    } else {
+        for (var i = from; i < items.length; i++) {
+            //actural reference of result list from crossref
+            var item_ref = items[i];
+            //construct info which needed to be return
+            var _title = !isNull(item_ref.title) ? item_ref.title[0] : "unkown";
+            var _author = !isNull(item_ref.author) ? construct_author(item_ref.author) : "unkown";
+            var _doi = !isNull(item_ref.DOI) ? item_ref.DOI : "unkown";
+            returnValue.list.push({
+                title: _title,
+                author: _author,
+                doi: _doi
+            });
+        }
+        return returnValue;
+    }
+}
+
+//async function return promise
+async function get_worklist(keyword, rows, offset, date) {
+    if (offset % cache == 0) {
+        //convert keyword in this format list : keyword1+keyword2+...
+        keyword = keyword.split(" ");
+        keyword = keyword.join("+");
+        let datefrom = date.from.getFullYear() + "-" +
+            ((date.from.getMonth() + 1) < 10 ? "0" : "") + (date.from.getMonth() + 1) +
+            "-" + (date.from.getDate() < 10 ? "0" : "") + date.from.getDate();
+        let dateto = date.to.getFullYear() + "-" +
+            ((date.to.getMonth() + 1) < 10 ? "0" : "") + (date.to.getMonth() + 1) + "-" +
+            (date.to.getDate() < 10 ? "0" : "") + date.to.getDate();
+        var search_url = url + keyword + "&filter=from-update-date:" + datefrom +
+            ",until-update-date:" + dateto +
+            "&rows=" + cache + "&offset=" + offset;
+        console.log(search_url);
+        let returnValue = {
+            list: [],
+            length: 0
+        };
+        return axios.get(search_url).then(res => {
+            //get reference : res -> data -> message
+            let ref = res.data.message;
+            if (!isNull(ref)) {
+                //save current 100 results in items as a list
+                items = ref.items;
+                returnValue = cons_returnValue(returnValue, offset % cache, offset % cache + rows);
+                returnValue.length = ref["total-results"];
+                result_length = returnValue.length;
+                console.log("结果数目：" + result_length);
+            }
+            return returnValue;
+        }).catch(err => {
+            console.log(err);
+        });
+    } else {
+        let returnValue = {
+            list: [],
+            length: 0
+        };
+        returnValue = cons_returnValue(items, returnValue, offset % cache, offset % cache + rows);
+        returnValue.length = result_length;
+        return returnValue;
+    }
 }
 
 const actions = {
@@ -82,7 +118,6 @@ const actions = {
         //commit('setlist',list)
         console.log("keyword is : " + keyword);
         //give the first 10 information(Todo), can reuse changepage
-        last_keyword = keyword;
         let pagefrom = 0;
         let pageto = 10;
         if (from)
@@ -90,7 +125,6 @@ const actions = {
         if (to)
             pageto = to;
         //wait inorder to know the setlest and setset will not earlier then them
-        console.log(date);
         let returnValue = await get_worklist(keyword, pageto - pagefrom, pagefrom, date);
         commit('setlist', returnValue.list);
         return returnValue;
